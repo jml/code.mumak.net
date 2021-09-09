007a894797c355fabfcc7433f65553a208caaaee
Jonathan Lange <jml@mumak.net>
2021-09-09 14:56:15 +0100

    Logo is absolute path

--- pelicanconf.py
+++ pelicanconf.py
@@ -6,7 +6,7 @@ AUTHOR = 'Jonathan Lange'
 COPYRIGHT = u'Jonathan M. Lange'
 SITENAME = u'jml :: Jonathan Lange'
 SITEURL = ''
-SITELOGO = 'images/jml-2019.jpg'
+SITELOGO = '/images/jml-2019.jpg'
 
 PATH = 'content'
 
